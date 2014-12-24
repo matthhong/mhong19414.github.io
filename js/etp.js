91dc89356282b78b419699eaeaf60a8756ff8541
Matt Hong <mhong19414@gmail.com>
2014-12-23 23:18:30 -0600

    rewriting the vis code

--- js/etp.js
+++ js/etp.js
@@ -1,45 +1,148 @@
-var save = function(){
-	force.stop();
-
-	var nodes_dl = 'data:text/json;charset=utf8,' + encodeURIComponent(JSON.stringify(force.nodes()));
-	var links_dl = 'data:text/json;charset=utf8,' + encodeURIComponent(JSON.stringify(force.links()));
-
-	$('<a href="' + nodes_dl + '" download="' + game + 'nodes.json">download nodes</a>').appendTo('#container');
-	$('<a href="' + links_dl + '" download="' + game + 'links.json">download links</a>').appendTo('#container');
-}
-
 $(function(){
-var first_game= '2011779-1'
 
-var force = d3.layout.force()
-		.charge(-120)
-		.linkDistance(200)
-		.size([546, 726]);
+$.ajaxSetup({
+    async: false
+});
 
-var svg = d3.select('svg').attr('width', 546).attr('height', 726);
+var FIRST_GAME = '2011779-1';
 
-svg.append('defs')
-		.append("svg:marker")
-	    .attr("id", "arrowGray")
-	    .attr("viewBox", "0 0 10 10")
-	    .attr("refX", "10")
-	    .attr("refY", "5")
-	    .attr("markerUnits", "userSpaceOnUse")
-	    .attr("markerWidth", "25")
-	    .attr("markerHeight", "25")
-	    .attr("orient", "auto")
-	    .append("svg:path")
-	    .attr("d", "M 7 5 L 10 5 L 0 10 z")
-	    .attr("fill", "#000");
+var W = 546; //width
+var H = 726; //height
+var R = 20; //node radius
 
-var dblclick = function(d) {
-	d3.select(this).classed("fixed", d.fixed = false);
-};
+var svg = d3.select('svg').attr('width', W).attr('height', H);
+var svg_graph = svg.append('g').attr('class','graph');
 
-var dragstart = function(d){ 
-	d3.select(this).classed("fixed", d.fixed = true);
+// Links
+var links_data = {};
+$.getJSON(FIRST_GAME  + 'links.json', function(d){
+	links_data = d;
+});
+var links = svg_graph.selectAll(".link").data(links_data).enter().append('line');
+links.attr("class", "hidden");
 
-};
+svg.append('defs') //Arrows
+	.append("svg:marker")
+    .attr("id", "arrowGray")
+    .attr("viewBox", "0 0 10 10")
+    .attr("refX", "10")
+    .attr("refY", "5")
+    .attr("markerUnits", "userSpaceOnUse")
+    .attr("markerWidth", "25")
+    .attr("markerHeight", "25")
+    .attr("orient", "auto")
+    .append("svg:path")
+    .attr("d", "M 7 5 L 10 5 L 0 10 z")
+    .attr("fill", "#000");
+
+var max_w = Math.max.apply(Math, links_data.map(function(d) {return d.value;}))
+links.style('stroke', 'black')
+	.style('stroke-width', function(d) { return d.value * 10 / max_w + 1; })
+	.attr("marker-end", function(d) { return d.value === 0 ? "none" : "url(#arrowGray)" })
+
+links.attr("x1", function(d) { 
+		if (dy === 0) { return d.source.x; };
+
+		// shift the lines so that the weighted directed links are separate
+		var v = d.value * 10 / max_w + 1,
+			shift = v / 2 + 1,
+			dx = d.target.x - d.source.x,
+			dy = d.target.y - d.source.y,
+			sign = dx / Math.abs(dx);
+
+		sign2 = (dx/dy) / Math.abs(dx/dy);
+		sign *= (-1 * sign2)
+
+		var shiftx = Math.sqrt(Math.pow(shift,2)/(1 + Math.pow(dx/dy,2)));
+		return sign * shiftx + d.source.x; 
+	})
+    .attr("y1", function(d) { 
+    	if (dx === 0) { return d.source.y; };
+
+    	// shift the lines so that the weighted directed links are separate
+		var v = d.value * 10 / max_w + 1,
+			shift =  v / 2 + 1,
+			dx = d.target.x - d.source.x,
+			dy = d.target.y - d.source.y,
+			sign = dy / Math.abs(dy);
+
+		sign2 = (dx/dy) / Math.abs(dx/dy);
+		sign *= sign2
+
+		var shifty = Math.sqrt(Math.pow(shift,2)/(1 + Math.pow(dy/dx,2)));
+		return d.source.y + (sign * shifty); 
+	})
+    .attr("x2", function(d) {
+    	// point arrow at the edge of target
+    	var length = Math.sqrt(Math.pow(d.target.y - d.source.y, 2) + Math.pow(d.target.x - d.source.x, 2));
+	    var scale = (length - R) / length;
+	    var offset = (d.target.x - d.source.x) - (d.target.x - d.source.x) * scale;
+	    var targetx = d.target.x - offset;
+
+    	if (dy === 0) { return targetx; };
+
+    	// shift the lines so that the weighted directed links are separate
+		var v = d.value * 10 / max_w + 1,
+			shift = v / 2 + 1,
+			dx = targetx - d.source.x,
+			dy = d.target.y - d.source.y,
+			sign = dx / Math.abs(dx);
+
+		sign2 = (dx/dy) / Math.abs(dx/dy);
+		sign *= (-1 * sign2)
+
+		var shiftx = Math.sqrt(Math.pow(shift,2)/(1 + Math.pow(dx/dy,2)));
+
+		return sign * shiftx + targetx;
+	})
+    .attr("y2", function(d) {
+    	// point arrow at the edge of target
+    	var length = Math.sqrt(Math.pow(d.target.y - d.source.y, 2) + Math.pow(d.target.x - d.source.x, 2));
+	    var scale = (length - R) / length;
+	    var offset = (d.target.y - d.source.y) - (d.target.y - d.source.y) * scale;
+	    var targety = d.target.y - offset;
+
+    	if (dx === 0) { return targety; };
+
+    	// shift the lines so that the weighted directed links are separate
+		var v = d.value * 10 / max_w + 1,
+			shift =  v / 2 + 1,
+			dx = d.target.x - d.source.x,
+			dy = targety - d.source.y,
+			sign = dy / Math.abs(dy);
+
+		sign2 = (dx/dy) / Math.abs(dx/dy);
+		sign *= sign2
+
+		var shifty = Math.sqrt(Math.pow(shift,2)/(1 + Math.pow(dy/dx,2)));
+
+		return targety + (sign * shifty); 
+	});
+
+// Nodes
+var nodes_data = {};
+$.getJSON(FIRST_GAME  + 'nodes.json', function(d){
+	nodes_data = d;
+});
+var nodes = svg_graph.selectAll(".node").data(nodes_data).enter().append("circle");
+
+nodes.attr("r", R)
+	.style("fill", 'blue')
+	.style('stroke', 'white')
+	.style('stroke-width', 2)
+
+nodes.attr("cx", function(d) { return d.x; })
+	.attr("cy", function(d) { return d.y; });
+
+// Labels
+var labels = svg.selectAll("text.label").data(nodes_data).enter().append("text");
+
+labels.attr("class", "label")
+    .attr("fill", "white")
+    .attr('font-family', "sans-serif")
+    .text(function(d) {  return d.name;  });
+            
+labels.attr("transform", function(d) { return "translate(" + (d.x - 6) + "," + (d.y + 6) + ")"; });
 
 var toggle_highlight = function(n1, n2){
 	var lines = d3.selectAll('line')
@@ -66,127 +169,38 @@ var clicked = function(d){
 	}
 }
 
-var group = svg.append('g').attr('class','pic').attr('width', 546).attr('height', 726);
-var nodeRadius = 20;
+nodes.on('click', clicked)
+labels.on('click', clicked);
 
-$.getJSON(first_game + 'links.json', function(data){
-	var max_w = Math.max.apply(Math, data.map(function(d) {return d.value;}))
-	var link = group.selectAll(".link").data(data).enter()
-		.append('line')
-		.attr("class", "hidden")
-		.style('stroke', 'black')
-		.style('stroke-width', function(d) { return d.value * 10 / max_w + 1; })
-		.attr("marker-end", function(d) { return d.value === 0 ? "none" : "url(#arrowGray)" })
+var force = d3.layout.force()
+	.charge(-120)
+	.linkDistance(200)
+	.size([W, H]);
 
-	link.attr("x1", function(d) { 
-			if (dy === 0) { return d.source.x; };
+var save = function(){
+	force.stop();
 
-			// shift the lines so that the weighted directed links are separate
-			var v = d.value * 10 / max_w + 1,
-				shift = v / 2 + 1,
-				dx = d.target.x - d.source.x,
-				dy = d.target.y - d.source.y,
-				sign = dx / Math.abs(dx);
+	var nodes_dl = 'data:text/json;charset=utf8,' + encodeURIComponent(JSON.stringify(force.nodes()));
+	var links_dl = 'data:text/json;charset=utf8,' + encodeURIComponent(JSON.stringify(force.links()));
 
-			sign2 = (dx/dy) / Math.abs(dx/dy);
-			sign *= (-1 * sign2)
+	$('<a href="' + nodes_dl + '" download="' + game + 'nodes.json">download nodes</a>').appendTo('#container');
+	$('<a href="' + links_dl + '" download="' + game + 'links.json">download links</a>').appendTo('#container');
+}
 
-			var shiftx = Math.sqrt(Math.pow(shift,2)/(1 + Math.pow(dx/dy,2)));
-			return sign * shiftx + d.source.x; 
-		})
-        .attr("y1", function(d) { 
-        	if (dx === 0) { return d.source.y; };
+var dblclick = function(d) {
+	d3.select(this).classed("fixed", d.fixed = false);
+};
 
-        	// shift the lines so that the weighted directed links are separate
-			var v = d.value * 10 / max_w + 1,
-				shift =  v / 2 + 1,
-				dx = d.target.x - d.source.x,
-				dy = d.target.y - d.source.y,
-				sign = dy / Math.abs(dy);
+var dragstart = function(d){ 
+	d3.select(this).classed("fixed", d.fixed = true);
 
-			sign2 = (dx/dy) / Math.abs(dx/dy);
-			sign *= sign2
-
-			var shifty = Math.sqrt(Math.pow(shift,2)/(1 + Math.pow(dy/dx,2)));
-			return d.source.y + (sign * shifty); 
-		})
-        .attr("x2", function(d) {
-        	var length = Math.sqrt(Math.pow(d.target.y - d.source.y, 2) + Math.pow(d.target.x - d.source.x, 2));
-		    var scale = (length - nodeRadius) / length;
-		    var offset = (d.target.x - d.source.x) - (d.target.x - d.source.x) * scale;
-		    var targetx = d.target.x - offset;
-
-        	if (dy === 0) { return targetx; };
-
-        	// shift the lines so that the weighted directed links are separate
-			var v = d.value * 10 / max_w + 1,
-				shift = v / 2 + 1,
-				dx = targetx - d.source.x,
-				dy = d.target.y - d.source.y,
-				sign = dx / Math.abs(dx);
-
-			sign2 = (dx/dy) / Math.abs(dx/dy);
-			sign *= (-1 * sign2)
-
-			var shiftx = Math.sqrt(Math.pow(shift,2)/(1 + Math.pow(dx/dy,2)));
-
-			return sign * shiftx + targetx;
-		})
-        .attr("y2", function(d) {
-        	var length = Math.sqrt(Math.pow(d.target.y - d.source.y, 2) + Math.pow(d.target.x - d.source.x, 2));
-		    var scale = (length - nodeRadius) / length;
-		    var offset = (d.target.y - d.source.y) - (d.target.y - d.source.y) * scale;
-		    var targety = d.target.y - offset;
-
-        	if (dx === 0) { return targety; };
-
-        	// shift the lines so that the weighted directed links are separate
-			var v = d.value * 10 / max_w + 1,
-				shift =  v / 2 + 1,
-				dx = d.target.x - d.source.x,
-				dy = targety - d.source.y,
-				sign = dy / Math.abs(dy);
-
-			sign2 = (dx/dy) / Math.abs(dx/dy);
-			sign *= sign2
-
-			var shifty = Math.sqrt(Math.pow(shift,2)/(1 + Math.pow(dy/dx,2)));
-
-			return targety + (sign * shifty); 
-		});
-
-	$.getJSON(first_game + 'nodes.json', function(data){
-
-	window.node = group.selectAll(".node").data(data).enter()
-		.append("circle")
-		.attr("r", nodeRadius)
-		.style("fill", 'blue')
-		.style('stroke', 'white')
-		.style('stroke-width', 2)
-		.on('click', clicked)
-
-	var text = svg.selectAll("text.label")
-                .data(data)
-                .enter().append("text")
-                .attr("class", "label")
-                .attr("fill", "white")
-                .attr('font-family', "sans-serif")
-                .text(function(d) {  return d.name;  })
-                .attr("transform", function(d) {
-			        return "translate(" + (d.x - 6) + "," + (d.y + 6) + ")";
-			    })
-			    .on('click', clicked);
-
-	node.attr("cx", function(d) { return d.x; })
-		    .attr("cy", function(d) { return d.y; });
-});
-});
+};
 
 var change = function(game){
 	var trans = [];
 
 	$.getJSON(game + 'nodes.json', function(data){
-		var node = group.selectAll('circle');
+		var node = svg_graph.selectAll('circle');
 		var p_nodes = node[0];
 
 		var dist_matrix = [];
@@ -224,7 +238,7 @@ var change = function(game){
 					seen_lines.push(i);
 				}
 			};
-			var link = group.selectAll("line").data(data)
+			var link = svg_graph.selectAll("line").data(data)
 				.transition()
 				.duration(3000)
 				.style('stroke', 'black')
@@ -272,7 +286,7 @@ var change = function(game){
 			})
 	        .attr("x2", function(d) {
 	        	var length = Math.sqrt(Math.pow(d.target.y - d.source.y, 2) + Math.pow(d.target.x - d.source.x, 2));
-			    var scale = (length - nodeRadius) / length;
+			    var scale = (length - R) / length;
 			    var offset = (d.target.x - d.source.x) - (d.target.x - d.source.x) * scale;
 			    var targetx = d.target.x - offset;
 
@@ -294,7 +308,7 @@ var change = function(game){
 			})
 	        .attr("y2", function(d) {
 	        	var length = Math.sqrt(Math.pow(d.target.y - d.source.y, 2) + Math.pow(d.target.x - d.source.x, 2));
-			    var scale = (length - nodeRadius) / length;
+			    var scale = (length - R) / length;
 			    var offset = (d.target.y - d.source.y) - (d.target.y - d.source.y) * scale;
 			    var targety = d.target.y - offset;
 
@@ -345,7 +359,7 @@ $('button').on('click', function(){
 })
 
 // $.getJSON('JSON-passes/' + game, function(data) {
-// 	var group = svg.append('g').attr('class','pic').attr('width', 546).attr('height', 726);
+// 	var svg_graph = svg.append('g').attr('class','graph').attr('width', W).attr('height', H);
 
 // 	var drag = force.drag().on("dragstart", dragstart);
 
@@ -369,18 +383,18 @@ $('button').on('click', function(){
 // 		.nodes(nodes)
 // 		.links(links)
 
-// 	var link = group.selectAll(".link").data(links).enter()
+// 	var link = svg_graph.selectAll(".link").data(links).enter()
 // 		.append('line')
 // 		.style('stroke', 'black')
 // 		.style('stroke-width', function(d) { return d.value / 1.5; })
 // 		.style('opacity', 0.7)
 // 		.attr("marker-end", "url(#arrowGray)");
 
-// 	var nodeRadius = 20;
+// 	var R = 20;
 
-// 	var node = group.selectAll(".node").data(nodes).enter()
+// 	var node = svg_graph.selectAll(".node").data(nodes).enter()
 // 		.append("circle")
-// 		.attr("r", nodeRadius)
+// 		.attr("r", R)
 // 		.style("fill", 'blue')
 // 		.style('stroke', 'white')
 // 		.style('stroke-width', 2)
@@ -430,7 +444,7 @@ $('button').on('click', function(){
 // 		})
 //         .attr("x2", function(d) {
 //         	var length = Math.sqrt(Math.pow(d.target.y - d.source.y, 2) + Math.pow(d.target.x - d.source.x, 2));
-// 		    var scale = (length - nodeRadius) / length;
+// 		    var scale = (length - R) / length;
 // 		    var offset = (d.target.x - d.source.x) - (d.target.x - d.source.x) * scale;
 // 		    var targetx = d.target.x - offset;
 
@@ -452,7 +466,7 @@ $('button').on('click', function(){
 // 		})
 //         .attr("y2", function(d) {
 //         	var length = Math.sqrt(Math.pow(d.target.y - d.source.y, 2) + Math.pow(d.target.x - d.source.x, 2));
-// 		    var scale = (length - nodeRadius) / length;
+// 		    var scale = (length - R) / length;
 // 		    var offset = (d.target.y - d.source.y) - (d.target.y - d.source.y) * scale;
 // 		    var targety = d.target.y - offset;
 
@@ -489,7 +503,7 @@ $('button').on('click', function(){
 // });
 
 // d3.xml(game +'.svg', 'image/svg+xml', function(xml) {
-// 	var group = svg.insert('g', '.pic').attr('class', 'graph')
+// 	var svg_graph = svg.insert('g', '.graph').attr('class', 'graph')
 // 	var external = xml.documentElement;
 // 	var svg_attrs = external.attributes;
 // 	var h = svg_attrs['height'].value,
@@ -500,12 +514,12 @@ $('button').on('click', function(){
 // 			var path_attrs = child_nodes[i].attributes,
 // 				style = path_attrs['style'].value,
 // 				d = path_attrs['d'].value;
-// 			group.attr('width', h).attr('height', w)
+// 			svg_graph.attr('width', h).attr('height', w)
 // 				.append('path').attr('d', d).attr('style', style)
 // 			if (+game[game.length-1] === 1){
-// 				group.attr('transform', 'translate(0 ' + w + ') rotate(270 0 0)');
+// 				svg_graph.attr('transform', 'translate(0 ' + w + ') rotate(270 0 0)');
 // 			}else{
-// 				group.attr('transform', 'translate(' + h + ' 0) rotate(90 0 0)');
+// 				svg_graph.attr('transform', 'translate(' + h + ' 0) rotate(90 0 0)');
 // 			}
 // 		}
 // 		catch(e){}
